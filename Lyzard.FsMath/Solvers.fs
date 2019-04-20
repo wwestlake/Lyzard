5f6dc0fcc279083f8e9fe8209f40eb70d5bdf3a5
Bill Westlake <wwestlake@lagdaemon.com>
2019-04-20 00:19:58 -0400

    Added Runge-Kutta and Euler ODE solvers in F# as well as C# to F# function format converters

---
+++ Lyzard.FsMath/Solvers.fs
@@ -0,0 +1,41 @@
+﻿namespace Lyzard.FsMath
+
+open System
+
+module Solvers =
+
+
+    /// Fourth order Runge-Kutta ODE Solver 
+    let RungeKutta4 x y h n f =
+        let half = 0.5
+        let sixth = 1.0/6.0
+        let two = 2.0
+        let rungekutta4' x y h f =
+            let k1 = h * f x y
+            let k2 = h * f (x + h * half) (y + k1 * half)
+            let k3 = h * f (x + h * half) (y + k2 * half)
+            let k4 = h * f (x + h) (y + k3)
+            y + sixth * (k1 + two * k2 + two * k3 + k4)
+
+        let rec inner x y h n f incr results =
+            let y' = rungekutta4' x y h f
+            if (x >= n) then (incr, results)
+            else inner (x+h) y' h n f (x+h :: incr) (y' :: results)
+        let (incr', result') = inner x y h n f [] [] 
+        List.zip incr' result' |> List.rev
+
+
+    /// Euler's first order solver for ODE's
+    let Euler f a b n y0 =
+        let h = (b - a) / float n
+        let rec euler' f x y0 h =
+            match x with
+            | x0::x1::xt -> let y1 = y0 + (f x0 y0) * h
+                            let yx = y0 + ((f x0 y0) + (f x1 y1)) / 2.0 * h
+                            printfn "%.06f %.06f %.06f" x1 y1 yx
+                            euler' f (x1::xt) yx h
+            | _ -> y0
+        euler' f [a..h..b] y0 h
+
+   
+
---
+++ Lyzard.UnitTests/FsMathTests.cs
@@ -0,0 +1,24 @@
+﻿using Lyzard.FsMath;
+using Microsoft.FSharp.Core;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lyzard.UnitTests
+{
+    [TestFixture]
+    public class FsMathTests
+    {
+        [Test]
+        public void RungeKutta_solver_test_simple_ODE()
+        {
+            var func = Converters.convert3<double,double,double>( (x, y) => Math.Exp(x) );
+            var result = Solvers.RungeKutta4(0.0, 1.0, 0.01, 1.0, func);
+            Assert.That(result.Length == 100);
+        }
+    }
+}

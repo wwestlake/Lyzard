cfd40ad77c71234141bb6e1098d975e010e87b3b
wwestlake <wwestlake@lagdaemon.com>
2019-04-24 07:06:03 -0400

    changed the mixxer to fix typo in division

--- Lyzard.FsMath/SignalProcessing.fs
+++ Lyzard.FsMath/SignalProcessing.fs
@@ -3,7 +3,7 @@
 module Mixxers =
 
     let mixxer a b =
-        Seq.zip a b |> Seq.map (fun (a,b) -> (a + b) / 3.0f)
+        Seq.zip a b |> Seq.map (fun (a,b) -> (a + b) / 2.0f)
 
     let clamp (min:float32) (max:float32) (a: float32 seq) =
         a |> Seq.map (fun x -> if x > max then max else if x < min then min else x )

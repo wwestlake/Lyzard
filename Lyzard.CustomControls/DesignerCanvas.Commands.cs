a66518616b8060a390b025e306921d3ec61e50a4
Bill Westlake <wwestlake@lagdaemon.com>
2019-05-27 13:47:16 -0400

    fixxed licensing on classes from sukram, the code project

--- Lyzard.CustomControls/DesignerCanvas.Commands.cs
+++ Lyzard.CustomControls/DesignerCanvas.Commands.cs
@@ -1,20 +1,8 @@
-﻿/* 
- * Lyzard Modeling and Simulation System
- * 
- * Copyright 2019 William W. Westlake Jr.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+﻿/*
+ * sukram: The Code Project Open License (CPOL)
+ * https://www.codeproject.com/Articles/22952/WPF-Diagram-Designer-Part-1
  */
+
 using Lyzard.Interfaces;
 using Microsoft.Win32;
 using System;

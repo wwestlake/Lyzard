e079574d9cd9f0c2a91f0ae5ac061612efa93793
William Westlake <wwestlake@lagdaemon.com>
2019-05-29 15:00:12 -0400

    building graphics rendering system

---
+++ Lyzard.Graphics/LineChart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+
+namespace Lyzard.GraphicsLib
+{
+    public class LineChart : Chart
+    {
+        public LineChart(GraphDescription description) : base(description)
+        {
+        }
+
+        protected override Bitmap RenderChart<T>(Graphics g, DataSet<T> dataset)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lyzard.Graphics/Renderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+
+namespace Lyzard.GraphicsLib
+{
+    public abstract class Renderer
+    {
+        public abstract void Render<T>(Graphics graphics, GraphDescription description, DataSet<T> dataset);
+    }
+
+    public class SolidBackgroundRenderer : Renderer
+    {
+        public override void Render<T>(Graphics graphics, GraphDescription description, DataSet<T> dataset)
+        {
+            var brush = new SolidBrush(description.Background);
+            graphics.FillRectangle(brush, description.Region);
+        }
+    }
+
+    public class AxisRender : Renderer
+    {
+        public override void Render<T>(Graphics graphics, GraphDescription description, DataSet<T> dataset)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
